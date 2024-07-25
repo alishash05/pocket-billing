9d363c2596fff6418ed62e6acdfd16f9512532b8
alishash05 <154660357+alishash05@users.noreply.github.com>
2024-07-25 05:56:51 -1200

    Update validation login.js

--- src/view/login/login.js
+++ src/view/login/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createUseStyles } from 'react-jss';
 import { useNavigate } from 'react-router-dom';
@@ -174,6 +174,116 @@ const login =() =>{
 </div>
 </div>
 
+  );
+}
+
+export default Login;
+*/
+
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createUseStyles } from 'react-jss';
+import { useNavigate } from 'react-router-dom';
+import { fetchLogin } from "../../service/loginSlice";
+
+const useStyles = createUseStyles({
+  loginPage: {
+    width: "360px",
+    padding: "8% 0 0",
+    margin: "auto",
+  },
+  form: {
+    position: "relative",
+    zIndex: 1,
+    background: "#FFFFFF",
+    maxWidth: "360px",
+    margin: "0 auto 100px",
+    padding: "45px",
+    textAlign: "center",
+    boxShadow: "0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24)",
+    "& input": {
+      fontFamily: "sans-serif",
+      outline: "0",
+      background: "#f2f2f2",
+      width: "100%",
+      border: "0",
+      margin: "0 0 15px",
+      padding: "15px",
+      boxSizing: "border-box",
+      fontSize: "14px",
+    },
+    "& button": {
+      fontFamily: "sans-serif",
+      textTransform: "uppercase",
+      outline: "0",
+      background: "#4CAF50",
+      width: "100%",
+      border: "0",
+      padding: "15px",
+      color: "#FFFFFF",
+      fontSize: "14px",
+      cursor: "pointer",
+    },
+    "& button:hover, & button:active, & button:focus": {
+      background: "#43A047",
+    },
+    message: {
+      margin: "15px 0 0",
+      color: "#b3b3b3",
+      fontSize: "12px",
+      fontFamily: "sans-serif",
+      "& a": {
+        color: "#4CAF50",
+        textDecoration: "none",
+      }
+    },
+  },
+});
+
+function Login() {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [userInput, setUserInput] = useState('');
+  const [passInput, setPassInput] = useState('');
+
+  const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  const onChange = (e) => {
+    const { id, value } = e.target;
+    // validation of username password is here 
+ 
+    if (id === "username") {
+      setUserInput(value);
+    } else if (id === "password") {
+      setPassInput(value);
+    }
+  }
+  console.log(userInput,"userInput");
+  console.log(passInput,"passInput");
+
+  const login = async () => {
+    if (!userInput.match(isValidEmail) || passInput === '') {
+      alert("Please Enter Correct Email and Password.");
+      return;
+    }
+//API call should be hit from here if error come should be diaplay in same page
+    await dispatch(fetchLogin(userInput, passInput));
+
+    navigate('/Next');
+  }
+
+  return (
+    <div className={classes.loginPage}>
+      <div className={classes.form}>
+        <form className={classes.loginForm}>
+          <input type="text" id="username" value={userInput} onChange={onChange} placeholder="Username" />
+          <input type="password" id="password" value={passInput} onChange={onChange} placeholder="Password" />
+          <button type="button" onClick={login}>Login</button>
+          <p className={classes.message}>Not registered? <a href="#">Create an account</a></p>
+        </form>
+      </div>
+    </div>
   );
 }
 
